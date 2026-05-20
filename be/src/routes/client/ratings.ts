4e4c765138c683211d647ebd2c1a2bf9e6a62510
chris <chris552611@gmail.com>
2026-05-21 02:20:21 +0800

    refactor: remove ratings feature (routes + service)

--- be/src/routes/client/ratings.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono'
-
-const app = new Hono()
-  .get('/eligible', c => c.json({ todo: 'rating-eligible bookings' }, 501))
-  .get('/mine', c => c.json({ todo: 'own ratings (incl. past edit window)' }, 501))
-  .post('/:bookingId', c => c.json({ todo: 'submit rating' }, 501))
-  .patch('/:bookingId', c => c.json({ todo: 'edit rating within window' }, 501))
-
-export default app
--- be/src/routes/portal/admin/ratings.ts
+++
@@ -1,7 +0,0 @@
-import { Hono } from 'hono'
-
-const app = new Hono()
-  .get('/', c => c.json({ todo: 'all ratings with full attribution' }, 501))
-  .get('/instructor/:id/aggregate', c => c.json({ todo: 'instructor aggregate average + count' }, 501))
-
-export default app
